a4e90e88e6a36f78552e6dfab3537e1d426c384b
Matt Thomas <matthew_thomas@hms.harvard.edu>
2019-01-17 14:44:05 -0500

    parts a and c working

--- more03/more03.c
+++ more03/more03.c
@@ -4,6 +4,7 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include "termfuncs.h"
 #define  PAGELEN	24
 #define  ERROR		1
 #define  SUCCESS	0
@@ -11,7 +12,7 @@
 #define	CTL_DEV	"/dev/tty"		/* source of control commands	*/
 
 int  do_more(FILE *);
-int  how_much_more(FILE *);
+int  how_much_more(FILE *, int);
 void print_one_line(FILE *);
 
 int main( int ac , char *av[] )
@@ -37,18 +38,32 @@ int main( int ac , char *av[] )
  */
 int do_more( FILE *fp )
 {
-	int	space_left = PAGELEN ;		/* space left on screen */
+	int rows = PAGELEN;
+	int cols = 80;
+	int rows_cols[2];
+	if ( get_term_size(rows_cols) == 0 )
+	{
+		rows = rows_cols[0];
+		cols = rows_cols[1];
+	}
+		
+	
+//	printf("window rows == %d\nwindow cols == %d\n", rows_cols[0], rows_cols[1]);
+
+	int	space_left = rows ;		/* space left on screen */
 	int	reply;				/* user request		*/
 	FILE	*fp_tty;			/* stream to keyboard	*/
 
 	fp_tty = fopen( CTL_DEV, "r" );		/* connect to keyboard	*/
 	while ( has_more_data( fp ) ) {		/* more input	*/
-		if ( space_left <= 0 ) {		/* screen full?	*/
-			reply = how_much_more(fp_tty);	/* ask user	*/
+		if ( space_left <= 1 ) {		/* screen full?	*/
+			reply = how_much_more(fp_tty, rows);	/* ask user	*/
 			if ( reply == 0 )		/*    n: done   */
 				break;
 			space_left = reply;		/* reset count	*/
+			printf("\033[7D");		//go back 7 columns (start of line)
 		}
+		
 		print_one_line( fp );
 		space_left--;				/* count it	*/
 	}
@@ -71,17 +86,18 @@ void print_one_line( FILE *fp )
  *      rets: number of additional lines to show: 0 => all done
  *	note: space => screenful, 'q' => quit, '\n' => one line
  */
-int how_much_more(FILE *fp)
+int how_much_more(FILE *fp, int rows)
 {
 	int	c;
 
 	printf("\033[7m more? \033[m");		/* reverse on a vt100	*/
-	while( (c = getc(fp)) != EOF )		/* get user input	*/
+	//while( (c = getc(fp)) != EOF )		/* get user input	*/
+	while( (c = rawgetc(fp)) != EOF )
 	{
 		if ( c == 'q' )			/* q -> N		*/
 			return 0;
 		if ( c == ' ' )			/* ' ' => next page	*/
-			return PAGELEN;		/* how many to show	*/
+			return rows;		/* how many to show	*/
 		if ( c == '\n' )		/* Enter key => 1 line	*/
 			return 1;		
 	}
