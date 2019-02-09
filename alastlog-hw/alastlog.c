44413cb757a4cc61c6ead6d7731e3b41062fd6ae
Matthew Thomas <matthew_thomas@hms.harvard.edu>
2019-02-09 12:42:26 -0500

    cleaned up options, print functions

--- alastlog-hw/alastlog.c
+++ alastlog-hw/alastlog.c
@@ -9,6 +9,7 @@
 #include <pwd.h>
 #include "lllib.h"
 
+void get_log(char *, char *, char *);
 void show_time(time_t, char *);
 void process(char *user, int days, char *file);
 void process_option(char *, char*, char*, int, char*);
@@ -17,94 +18,108 @@ void show_llrec(struct lastlog *);
 void show_info(struct lastlog *, struct passwd *);
 
 #define LLOG_FILE "/var/log/lastlog"
+#define TIME_FORMAT "%a %b %d %H:%M:%S %z %Y"
 
 int main (int ac, char *av[])
 {
-	int i = 1;
-	char user[UT_LINESIZE];
-	int days;
-	char file[255];
-
-	while (i < ac)
-	{
-		if(av[i][0] == '-')
+    int i = 1;
+    char *user = NULL;
+    char *days = NULL;
+    char *file = NULL;
+    
+    /*Cycle through command-line args to store any options user specifies.
+      Known options are -u, -t, and -f. Each option requires an arg following
+      it.*/
+    while (i < ac)
+    {
+		if(av[i][0] == '-' && (i + 1) < ac && av[i+1][0] != '-')
 		{
 			if(av[i][1] == 'u')
-				strcpy(user, av[i+1]);
-//			else if(av[i][1] == 't')
-//				days = av[i+1];
+                user = av[i+1];
+			else if(av[i][1] == 't')
+				days = av[i+1];
 			else if (av[i][1] == 'f')
-				strcpy(file, av[i+1]);
-
+                file = av[i+1];
+            else
+                printf("Unknown option. Please try again.\n");
 		}
 		else
 			printf("usage: ./alastlog [-utf] [file]\n");
 
-//			process_option(av[i], av[i+1], &user, &days, &file);
-
 		i += 2;
 	}
 
-//	printf("user is %s, days is %d, and file is %s\n", user, days, file);
+    //If no file specified with -f, use LLOG_FILE
+    if (file == NULL)
+        get_log(LLOG_FILE, user, days);
+    else
+        get_log(file, user, days);
+
+	return 0;
+}
+
+void get_log(char *file, char *user, char *days)
+{
+	//printf("user is %s, days is %d, and file is %s\n", user, atoi(days), file);
 
 	struct lastlog llbuf;
 	int llfd;
 
-	if( (llfd = open(LLOG_FILE, O_RDONLY)) == -1 )
+	if( (llfd = open(file, O_RDONLY)) == -1 )
 	{
-		fprintf(stderr, "%s: cannout open %s\n", *av, "/var/log/lastlog");
+		fprintf(stderr, "%s: cannout open %s\n", file, "/var/log/lastlog");
 		exit(1);
 	}
 	struct passwd *entry;
 //	printf("should be open, fd was %d\n", llfd);
-	printf("%-16s %-8s %-16s %-1s\n", "Username", "Part", "From", "Latest");
+//	printf("%-16s %-8s %-16s %-1s\n", "Username", "Part", "From", "Latest");
+    print_headers();
+
 	while( read(llfd, &llbuf, sizeof(llbuf)) == sizeof(llbuf) )
 	{
 //		printf("read data\n");
 		entry = getpwent();
-		show_info(&llbuf, &entry);
+		show_info(&llbuf, entry);
 	}
-	close(llfd);
-	return 0;
 
-//	process(user, days, file);
+    endpwent();
+    close(llfd);
+    return;
+}
 
-//	struct lastlog ll = { 0 };
-//	read_lastlog(&ll);
-//	fprintf(stdout, "%d %10s %10s\n", ll.ll_time, ll.ll_line, ll.ll_host);
-//	return 0;
+void print_headers()
+{
+	printf("%-16.16s ", "Username");
+	printf("%-8.8s ", "Port");
+	printf("%-16.16s", "From");
+    printf("%s", "Latest");
+    printf("\n");
+    return;
 }
 
 void show_info(struct lastlog *lp, struct passwd *ep)
 {
-//	printf("lp ut_type is %d\n", lp->ut_type);
-//	printf("in show_info...\n");
-//	printf("%-16s %-8s %-16s %-1d\n", lp->ll_line, lp->ll_host, lp->ll_time);
 	printf("%-16.16s ", ep->pw_name);
 	printf("%-8.8s ", lp->ll_line);
-	printf("%-16.16s", lp->ll_host);
-//	printf("%-16.16s %-8.8s %-16.16s ", "", lp->ll_line, lp->ll_host);
+	printf("%-16.16s ", lp->ll_host);
 
 	if(lp->ll_time == 0)
 		printf("**Never logged in**");
 	else
-		show_time(lp->ll_time, "%a %b %d %H:%M:%S %z %Y");
-	//	printf("%-1d\n", lp->ll_time);
+		show_time(lp->ll_time, TIME_FORMAT);
 
 	printf("\n");
-//	fprintf(stdout, "%d %10s %10s\n", lp->ll_time, lp->ll_line, lp->ll_host);
+
 	return;
 }
 
-void show_time(time_t value, char *fmt)
+void show_time(time_t time, char *fmt)
 {
+	struct tm *tp = localtime(&time);
 	char result[100];
 
-//	char *time = ctime(value);
-
-	struct tm *tp = localtime(&value);
 	strftime(result, 100, fmt, tp);
-//	fputs(result, stdout);
+
 	printf("%s", result);
 	return;
 }
